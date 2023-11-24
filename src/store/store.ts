b7ca3c9344ea16c79570c58b48dc55ac2155db50
Paula <andronicapaula25@gmail.com>
2023-11-24 17:40:31 +0200

    remove redux-thunk from the project

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk'; // For asynchronous actions
 import usersReducer from '../reducers/usersReducer';
 
 export const store = configureStore({
   reducer: usersReducer,
-  middleware: [thunk],
 });
